a144105669bbf479eaaf956afac042be13cce155
Arseniy <arsenit.developer@gmail.com>
2021-01-27 21:50:15 +0300

    fixed RealmProduct

--- OnlineShop/OnlineShop/Model/RealmProduct.swift
+++ OnlineShop/OnlineShop/Model/RealmProduct.swift
@@ -5,23 +5,21 @@
 //  Created by Arseniy on 27.01.2021.
 //
 
+import Foundation
 import RealmSwift
 
-class RealmProduct: Object {
+final class RealmProduct: Object {
    @objc dynamic var name: String = ""
    @objc dynamic var price: Double = 0.0
    @objc dynamic var rating: Float = 0.0
    @objc dynamic var imageUrl: String = ""
     
-    init(product: Product) {
+    convenience init(product: Product) {
+        self.init()
         self.name = product.name ?? ""
         self.price = product.price ?? 0.0
         self.rating = product.rating ?? 0.0
         self.imageUrl = product.imageUrl ?? ""
-    }
-    
-    override required init() {
-        fatalError("init() has not been implemented")
     }
     
     @objc override class func primaryKey() -> String? {
