e0cf2f52b793a6704926fc46749663a1934b2a51
Arseniy <arsenit.developer@gmail.com>
2021-01-27 18:19:40 +0300

    added CatalogLayoutCalculator

--- OnlineShop/OnlineShop/Objects/CatalogLayoutCalculator.swift
+++ OnlineShop/OnlineShop/Objects/CatalogLayoutCalculator.swift
@@ -5,4 +5,24 @@
 //  Created by Arseniy on 27.01.2021.
 //
 
-import Foundation
+import UIKit
+
+struct CatalogLayoutCalculator {
+    private let spacing: CGFloat
+    private let numberOfColumns: CGFloat
+    private let aspectRatio: CGFloat
+
+    init(spacing: CGFloat, numberOfColumns: Int, aspectRatio: CGFloat) {
+        self.spacing = spacing
+        self.numberOfColumns = CGFloat(numberOfColumns)
+        self.aspectRatio = aspectRatio
+    }
+
+    func calculateSize(for mainWidth: CGFloat) -> CGSize {
+        let width = (mainWidth - spacing * 2 - spacing * (numberOfColumns - 1)) / numberOfColumns
+        let height = width * aspectRatio
+
+        let size: CGSize = .init(width: width, height: height)
+        return size
+    }
+}
