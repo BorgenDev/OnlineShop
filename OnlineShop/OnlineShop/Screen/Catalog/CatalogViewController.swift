07be9f902521cfc02c14e96c3cd1f18afad92b6c
Arseniy <arsenit.developer@gmail.com>
2021-01-27 18:08:11 +0300

    registered ProductCollectionViewCell

--- OnlineShop/OnlineShop/Screen/Catalog/CatalogViewController.swift
+++ OnlineShop/OnlineShop/Screen/Catalog/CatalogViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 class CatalogViewController: UIViewController {
     
     @IBOutlet private weak var collectionView: UICollectionView!
+    var presenter: CatalogPresenter?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        collectionView.register(ProductCollectionViewCell.nib, forCellWithReuseIdentifier: ProductCollectionViewCell.nibName)
     }
 
 }
