f69f140632d372966ff03cae8db36151c8654c70
Arseniy <arsenit.developer@gmail.com>
2021-01-27 17:39:03 +0300

    added model Product

---
+++ OnlineShop/OnlineShop/Model/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  OnlineShop
+//
+//  Created by Arseniy on 27.01.2021.
+//
+
+import Foundation
+struct Product: Codable {
+    var name: String?
+    var price: Double?
+    var rating: Float?
+    var imageUrl: String?
+}
