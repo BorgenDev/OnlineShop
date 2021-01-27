1fab89ea90acd5f1c4baa9888d636222abf61fa6
Arseniy <arsenit.developer@gmail.com>
2021-01-27 18:25:22 +0300

    create CatalogAssembly

---
+++ OnlineShop/OnlineShop/Assembly/CatalogAssembly.swift
@@ -0,0 +1,21 @@
+//
+//  CatalogAssembly.swift
+//  OnlineShop
+//
+//  Created by Arseniy on 27.01.2021.
+//
+
+import Foundation
+class CatalogAssembly {
+    func assembly() -> UIViewController {
+        let vc = CatalogViewController()
+        let presenter = CatalogPresenter()
+        let interactor = CatalogInteractor()
+
+        vc.presenter = presenter
+        presenter.view = vc
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        return vc
+    }
+}
