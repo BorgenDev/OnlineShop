007337f5fa0c481c64a2c591e0db30a033900340
Arseniy <arsenit.developer@gmail.com>
2021-01-28 16:52:40 +0300

    added and configured CartTableViewCell

---
+++ OnlineShop/OnlineShop/Custom views/Cells/TableView/CartTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  CartTableViewCell.swift
+//  OnlineShop
+//
+//  Created by Arseniy on 28.01.2021.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell, NibLoadable {
+    
+    @IBOutlet private weak var productView: UIImageView!
+    @IBOutlet private weak var nameLable: UILabel!
+    @IBOutlet private weak var subtitleLable: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        productView.backgroundColor = .gray
+        productView.layer.cornerRadius = 8
+        productView.contentMode = .scaleAspectFill
+    }
+
+    func configure(by product: Product) {
+        let source = URL(string: product.imageUrl ?? "")
+        productView.kf.setImage(with: source)
+        nameLable.text = product.name ?? ""
+        var subtitle = ""
+        if let price = product.price {
+            subtitle += "Цена: \(price)"
+        }
+        if let raiting = product.rating {
+            if !subtitle.isEmpty {
+                subtitle += "\n"
+            }
+            subtitle += "Рейтинг: \(Int(raiting * 100))%"
+        }
+        subtitleLable.text = subtitle
+    }
+    
+}
---
+++ OnlineShop/OnlineShop/Screen/Cart/CartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CartViewController.swift
+//  OnlineShop
+//
+//  Created by Arseniy on 28.01.2021.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
