6017d66f767f1ba4f11bea2f2ba8a57158015ade
Arseniy <arsenit.developer@gmail.com>
2021-01-30 18:25:59 +0300

    added containers in SceneDelegate

--- OnlineShop/OnlineShop/Support files/SceneDelegate.swift
+++ OnlineShop/OnlineShop/Support files/SceneDelegate.swift
@@ -11,16 +11,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    let mainContainer = MainContainer()
+    private lazy var containersController = ContainerController(mainContainer: mainContainer)
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        containersController.setupe()
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let catalogAssembly = CatalogAssembly()
+
+        let catalogAssembly = CatalogAssembly(container: mainContainer.rootContainer)
         let cartAssembly = CartAssembly()
-        let catalogViewController = catalogAssembly.assembly()
+        
+        guard let catalogViewController = catalogAssembly.assembly() else {
+            return
+        }
+        
         let cartViewController = cartAssembly.assembly()
         
         let catalogNC = UINavigationController(rootViewController: catalogViewController)
