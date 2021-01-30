68fac2a54c363ef7bad043f3b90236762c03cd52
Arseniy <arsenit.developer@gmail.com>
2021-01-30 16:58:16 +0300

    created Containerable protocol

---
+++ OnlineShop/OnlineShop/Protocols/Containerable.swift
@@ -0,0 +1,13 @@
+//
+//  Containerable.swift
+//  OnlineShop
+//
+//  Created by Arseniy on 30.01.2021.
+//
+
+import Foundation
+
+protocol Containerable {
+    init(mainContainer: MainContainer)
+    func register()
+}
