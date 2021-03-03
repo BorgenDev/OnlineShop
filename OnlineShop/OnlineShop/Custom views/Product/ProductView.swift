bbddc556c0dfd7dff9645e2512182ed8b3041855
Arseniy <arsenit.developer@gmail.com>
2021-03-03 20:18:40 +0300

    filled ProductView

--- OnlineShop/OnlineShop/Custom views/Product/ProductView.swift
+++ OnlineShop/OnlineShop/Custom views/Product/ProductView.swift
@@ -5,4 +5,35 @@
 //  Created by Arseniy on 03.03.2021.
 //
 
-import Foundation
+import UIKit
+
+class ProductView: UIView, NibLoadable {
+    @IBOutlet private weak var productView: UIImageView!
+    @IBOutlet private weak var nameLable: UILabel!
+    @IBOutlet private weak var subtitleLable: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        productView.backgroundColor = .gray
+        productView.layer.cornerRadius = 8
+        productView.contentMode = .scaleAspectFill
+    }
+    
+
+    func configure(by product: Product) {
+        let source = URL(string: product.imageUrl ?? "")
+        productView.kf.setImage(with: source)
+        nameLable.text = product.name ?? ""
+        var subtitle = ""
+        if let price = product.price {
+            subtitle += "Цена: \(price)"
+        }
+        if let raiting = product.rating {
+            if !subtitle.isEmpty {
+                subtitle += "\n"
+            }
+            subtitle += "Рейтинг: \(Int(raiting * 100))%"
+        }
+        subtitleLable.text = subtitle
+    }
+}
